8ce65c9d3f79712010ee3e1427b45293c4b8412f
qEmrys <takashiro.sv@gmail.com>
2026-03-04 17:43:52 +0200

    refactor command handling logic

--- bot_assistant.py
+++ bot_assistant.py
@@ -37,30 +37,35 @@ def show_phone(args: list, contacts: dict) -> str:
 def show_all(contacts: dict) -> str:
     return "\n".join(f"{name}: {phone}" for name, phone in contacts.items())
 
+def exit_command(_):
+    print("Good bye!")
+    raise SystemExit
+
 def main():
     contacts = {}
 
     print("Welcome to the assistant bot!")
 
+    command_list = {
+        "hello": lambda args: "How can I help you?",
+        "add": lambda args: add_contact(args, contacts),
+        "change": lambda args: change_contact(args, contacts),
+        "phone": lambda args: show_phone(args, contacts),
+        "all": lambda: show_all(contacts),
+        "exit": exit_command,
+        "close": exit_command
+    }
+
     while True:
         user_input = input("Enter a command: ")
         command, *args = parse_input(user_input)
+        handler = command_list.get(command)
 
-        if command in ["close", "exit"]:
-            print("Good bye!")
-            break
-        elif command == "hello":
-            print("How can I help you?")
-        elif command == "add":
-            print(add_contact(args, contacts))
-        elif command == "change":
-            print(change_contact(args, contacts))
-        elif command == "phone":
-            print(show_phone(args, contacts))
-        elif command == "all":
-            print(show_all(contacts))
-        else:
+        if handler:
+            print(handler(args))        
+        else:           
             print("Invalid command.")
+
 
 if __name__ == "__main__":
     main()
